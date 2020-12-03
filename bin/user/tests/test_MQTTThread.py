bf35de022e189b8fceca793e2f11c7b5686cafd1
Rich Bell <bellrichm@gmail.com>
2020-12-03 11:08:27 -0500

    additional init tests

--- bin/user/tests/test_MQTTThread.py
+++ bin/user/tests/test_MQTTThread.py
@@ -4,6 +4,7 @@ import ssl
 import string
 
 import unittest
+import mock
 
 import configobj
 
@@ -12,37 +13,36 @@ from user.mqtt import MQTTThread
 def random_string():
     return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
 
-class TestTLSInitialization(unittest.TestCase):
-    @staticmethod
-    def create_topic(skip_upload=False,
-                     binding='archive',
-                     aggregation='aggregate',
-                     append_units_label=True,
-                     augment_record=True,
-                     upload_all=True,
-                     retain=False,
-                     qos=0,
-                     inputs={},
-                     templates={}):
-        return {
-            'skip_upload': skip_upload,
-            'binding': binding,
-            'aggregation': aggregation,
-            'append_units_label': append_units_label,
-            'augment_record': augment_record,
-            'upload_all': upload_all,
-            'retain': retain,
-            'qos': qos,
-            'inputs': inputs,
-            'templates': templates
-        }
+def create_topic(skip_upload=False,
+                 binding='archive',
+                 aggregation='aggregate',
+                 append_units_label=True,
+                 augment_record=True,
+                 upload_all=True,
+                 retain=False,
+                 qos=0,
+                 inputs={},
+                 templates={}):
+    return {
+        'skip_upload': skip_upload,
+        'binding': binding,
+        'aggregation': aggregation,
+        'append_units_label': append_units_label,
+        'augment_record': augment_record,
+        'upload_all': upload_all,
+        'retain': retain,
+        'qos': qos,
+        'inputs': inputs,
+        'templates': templates
+    }
 
+class TestTLSInitialization(unittest.TestCase):
     def test_certs_required(self):
         site_dict = {
             'server_url' : random_string,
             'topics': {
-                'weather/loop': self.create_topic(),
-                'weather': self.create_topic(aggregation='individual')
+                'weather/loop': create_topic(),
+                'weather': create_topic(aggregation='individual')
             },
             'manager_dict': {
                 random_string(): random_string()
@@ -60,8 +60,8 @@ class TestTLSInitialization(unittest.TestCase):
         site_dict = {
             'server_url' : random_string,
             'topics': {
-                'weather/loop': self.create_topic(),
-                'weather': self.create_topic(aggregation='individual')
+                'weather/loop': create_topic(),
+                'weather': create_topic(aggregation='individual')
             },
             'manager_dict': {
                 random_string(): random_string()
@@ -80,8 +80,8 @@ class TestTLSInitialization(unittest.TestCase):
         site_dict = {
             'server_url' : random_string,
             'topics': {
-                'weather/loop': self.create_topic(),
-                'weather': self.create_topic(aggregation='individual')
+                'weather/loop': create_topic(),
+                'weather': create_topic(aggregation='individual')
             },
             'manager_dict': {
                 random_string(): random_string()
@@ -95,9 +95,107 @@ class TestTLSInitialization(unittest.TestCase):
         SUT = MQTTThread(None, **site_config)
         self.assertEqual(SUT.tls_dict, {'ca_certs': ca_certs})
 
+class TestPersistentConnection(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestPersistentConnection, self).__init__(*args, **kwargs)
+        self.client_connection = None
+        self.connection_tries = 0
+        self.max_connection_tries = random.randint(1, 3)
+
+    def reset_connection_error(self, *args, **kwargs): # match signature pylint: disable=unused-argument
+        if self.connection_tries >= self.max_connection_tries:
+            self.client_connection.side_effect = mock.Mock(side_effect=None)
+        self.connection_tries += 1
+        return mock.DEFAULT
+
+    def test_connection_error(self):
+        max_tries = random.randint(4, 6)
+        site_dict = {
+            'persist_connection': True,
+            'max_tries': max_tries,
+            'server_url' : 'mqtt://username:password@localhost:1883/',
+            'topics': {
+                'weather/loop': create_topic(),
+                'weather': create_topic(aggregation='individual')
+            },
+            'manager_dict': {
+                random_string(): random_string()
+            }
+        }
+        site_config = configobj.ConfigObj(site_dict)
+
+        exception = ConnectionRefusedError("Connect exception.")
+
+        with mock.patch('paho.mqtt.client.Client') as mock_client:
+            with mock.patch('user.mqtt.time') as mock_time:
+                mock_client.return_value = mock_client
+                mock_client.connect.side_effect = mock.Mock(side_effect=exception)
+
+                with self.assertRaises(ConnectionError) as error:
+                    MQTTThread(None, **site_config)
+
+                self.assertEqual(len(error.exception.args), 0)
+                self.assertEqual(mock_client.connect.call_count, max_tries)
+                self.assertEqual(mock_time.sleep.call_count, max_tries)
+
+    def test_connection_recovers(self):
+        site_dict = {
+            'persist_connection': True,
+            'max_tries': random.randint(4, 6),
+            'server_url' : 'mqtt://username:password@localhost:1883/',
+            'topics': {
+                'weather/loop': create_topic(),
+                'weather': create_topic(aggregation='individual')
+            },
+            'manager_dict': {
+                random_string(): random_string()
+            }
+        }
+        site_config = configobj.ConfigObj(site_dict)
+
+        exception = ConnectionRefusedError("Connect exception.")
+        self.connection_tries = 0
+
+        with mock.patch('paho.mqtt.client.Client') as mock_client:
+            with mock.patch('user.mqtt.time') as mock_time:
+                mock_client.return_value = mock_client
+                mock_client.connect.side_effect = mock.Mock(side_effect=exception)
+                self.client_connection = mock_client.connect
+                mock_time.sleep.side_effect = self.reset_connection_error
+
+                MQTTThread(None, **site_config)
+
+                self.assertEqual(mock_client.connect.call_count, self.connection_tries + 1)
+                self.assertEqual(mock_time.sleep.call_count, self.connection_tries)
+
+    def test_connection_success(self):
+        site_dict = {
+            'persist_connection': True,
+            'max_tries': random.randint(4, 6),
+            'server_url' : 'mqtt://username:password@localhost:1883/',
+            'topics': {
+                'weather/loop': create_topic(),
+                'weather': create_topic(aggregation='individual')
+            },
+            'manager_dict': {
+                random_string(): random_string()
+            }
+        }
+        site_config = configobj.ConfigObj(site_dict)
+
+        self.connection_tries = 0
+
+        with mock.patch('paho.mqtt.client.Client') as mock_client:
+            with mock.patch('user.mqtt.time') as mock_time:
+                mock_client.return_value = mock_client
+                MQTTThread(None, **site_config)
+
+                self.assertEqual(mock_client.connect.call_count, 1)
+                self.assertEqual(mock_time.sleep.call_count, 0)
+
 if __name__ == '__main__':
     #test_suite = unittest.TestSuite()
-    #test_suite.addTest(TestTLSInitialization('test_new'))
+    #test_suite.addTest(TestPersistentConnection('test_new'))
     #unittest.TextTestRunner().run(test_suite)
 
     unittest.main(exit=False)
